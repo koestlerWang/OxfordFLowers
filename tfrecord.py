915a5ae1dcef534f0ee3874d356dd7dab28e456c
koestlerWang <1272599042@qq.com>
2019-03-13 22:04:03 +0800

    Update tfrecord.py

--- tfrecord.py
+++ tfrecord.py
@@ -12,8 +12,8 @@ import tensorflow as tf
 path="C://Users//wgh//Desktop//Oxford//jpg//"
 classes=['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 #classes=['0','1']
-writer_train = tf.python_io.TFRecordWriter("C://Users//wgh//Desktop//Oxford//jpg//train.tfrecords")#将每个图片集的前六十张图片作为训练集
-writer_test  = tf.python_io.TFRecordWriter("C://Users//wgh//Desktop//Oxford//jpg//test.tfrecords")#将每个图片集的后二十张图片作为测试集
+writer_train = tf.python_io.TFRecordWriter("C://Users//wgh//Desktop//Oxford//jpg//oxfordpicture.tfrecords")
+#writer_test  = tf.python_io.TFRecordWriter("C://Users//wgh//Desktop//Oxford//jpg//test.tfrecords")可以将测试集训练集单独生成tfrecord也可以不区分
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -25,33 +25,21 @@ def _bytes_feature(value):
 with tf.Session() as sess:
     for index,name in enumerate(classes):
         class_path=path+name+'//'
-        iterations=0
         for imagename in os.listdir(class_path):
-            iterations=iterations+1
-            if(iterations<=60):
-                imag_path=class_path+imagename
-                image = tf.read_file(imag_path)
-                image = tf.image.decode_jpeg(image, channels=3)
-                image_raw=(image.eval()).tostring()
-                example=tf.train.Example(features=tf.train.Features(feature={
+            imag_path=class_path+imagename
+            image = tf.read_file(imag_path)
+            image = tf.image.decode_jpeg(image, channels=3)
+            image_raw=(image.eval()).tostring()
+            example=tf.train.Example(features=tf.train.Features(feature={
                             'label':_int64_feature(index),
                             'image_raw':_bytes_feature(image_raw)
                             }
                     ))
-                writer_train.write(example.SerializeToString())
-            elif(iterations>60):
-                imag_path=class_path+imagename
-                image = tf.read_file(imag_path)
-                image = tf.image.decode_jpeg(image, channels=3)
-                image_raw=(image.eval()).tostring()
-                example=tf.train.Example(features=tf.train.Features(feature={
-                            'label':_int64_feature(index),
-                            'image_raw':_bytes_feature(image_raw)
-                            }
-                    ))
-                writer_test.write(example.SerializeToString())
+            writer_train.write(example.SerializeToString())
+           
+               
     writer_train.close()
-    writer_test.close()
+
     
 #"C://Users//wgh//Desktop//Oxford//jpg//output.tfrecords"
 
